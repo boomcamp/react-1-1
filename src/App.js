75a6d2166635c6824a2c605fe9728dddd81ab54f
Sid <bercasio.sidney@gmail.com>
2019-06-19 14:06:02 +0800

    Done Part 1 Step 2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,33 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {}
+export default class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            products : 
+            [{
+                id: 1,
+                imageUrl: 'https://via.placeholder.com/150x150',
+                title: 'House & Lot',
+                price: 100,
+                description: 'With furnitures inside',
+            },
+            {
+                id: 2,
+                imageUrl: 'https://via.placeholder.com/150x150',
+                title: 'Red Car',
+                price: 90,
+                description: 'With free 1 gallon of gas',
+            },
+            {
+                id: 1,
+                imageUrl: 'https://via.placeholder.com/150x150',
+                title: 'Wife',
+                price: 10,
+                description: 'Have 4 babies',
+            }],
+            cart: [],
+        };
+    }
+    
+}
