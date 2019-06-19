af09ab1f572103fa6fa2c3f515b09b293657c5a8
marvin banton <marvin.banton@boom.camp>
2019-06-19 16:23:53 +0800

    Done Step 5

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
                     imageUrl: 'https://via.placeholder.com/150x150',
                     title: 'Yamaha Sniper 150',
                     price: 99000,
-                    description: 'racing motor design.',   
+                    description: 'Gixxer is a comfortable bike with excellent manoeuvrability. The motorcycles large diameter front forks aid in reducing weight and contributes to sporty styling.',   
                 },
                 {
                     id: 2,
@@ -34,18 +34,48 @@ export default class App extends Component {
                     description: 'The Fortuner is a 7 seater SUV and has a length of 4795mm, width of 1855mm and a wheelbase of 2745m.',
                     
                 },
+                {
+                    id: 5,
+                    imageUrl: 'https://via.placeholder.com/150x150',
+                    title: 'Toyota Hi-ace',
+                    price: 2900000,
+                    description: 'The Fortuner is a 7 seater SUV and has a length of 4795mm, width of 1855mm and a wheelbase of 2745m.',
+                    
+                },
             ],
             cart: [],
         }
     }
+    
+    addToCart(item) {
+        this.setState({
+            cart: [...this.state.cart, item],
+        });
+    }
 
-    render () {
+    render() {
         return (
             <div className="App"> 
                 <section className="products">
                     <h1>Products</h1>
+                    {this.state.products.map(item =>(
+                        
+                        <div key={item.id} className="products">
+                            <div className="content">
+                                <div className="picture">
+                                    <img src={item.imageUrl} height="180" width="200" />
+                                </div>
+                                <div className="details">
+                                    <h4>{item.title}</h4>
+                                    <p>{item.price}</p>
+                                    <p>{item.description}</p>
+                                    <button onClick={() => this.addToCart(item)}>Add to Cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </section>
-
+                
                 <section className="cart">
                     <h1>Cart</h1>
                 </section>
