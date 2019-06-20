9be04d674ec7f29c794af93ea0082bb18346a2e6
Jules <jules.ballaran@boom.camp>
2019-06-20 10:15:53 +0800

    done

--- src/App.js
+++ src/App.js
@@ -13,14 +13,16 @@ export default class App extends Component {
 					description:
 						"Headgear commonly used by fishermen. Increases fishing skill marginally.",
 					price: 12.99,
-					imageUrl: "https://via.placeholder.com/150x150"
+					imageUrl: "https://via.placeholder.com/150x150",
+					quantity: 0,
 				},
 				{
 					id: 2,
 					title: "Metal Hat",
 					description: "Uncomfortable, but sturdy.",
 					price: 8.99,
-					imageUrl: "https://via.placeholder.com/150x150"
+					imageUrl: "https://via.placeholder.com/150x150",
+					quantity: 0,
 				}
 			],
 			beachGear: [
@@ -29,7 +31,8 @@ export default class App extends Component {
 					title: "Tent",
 					description: "Portable shelter.",
 					price: 32.99,
-					imageUrl: "https://via.placeholder.com/150x150"
+					imageUrl: "https://via.placeholder.com/150x150",
+					quantity: 0,
 				}
 			],
 			shirts: [],
@@ -39,21 +42,36 @@ export default class App extends Component {
 	}
 
 	addToCart = (item) => {
-		if(this.state.cart.find(ite => ite.id === item.id)){
-			let it = this.state.cart.find(ite => ite.id === item.id)
-			it.quantity += 1;
-			console.log(it)
-			this.setState({
-				cart : this.state.cart.concat(it)
-			})
-		}
-		else{
+		// if(this.state.cart.find(ite => ite.id === item.id)){
+		// 	let id = this.state.cart.findIndex(ite => ite.id === item.id)
+		// 	it.quantity += 1;
+		// 	console.log(it)
+		// 	this.setState({
+		// 		cart : it
+		// 	})
+		// }
+		// else{
+		// 	item.quantity = 1;
+		// 	console.log(item);
+		// 	this.setState({
+		// 		cart: this.state.cart.concat(item)
+		// 	})
+		// }
+		let cartCopy = this.state.cart.map(product => Object.assign({}, product));
+		console.log(cartCopy)
+		let i = this.state.cart.findIndex(ite => ite.id === item.id)
+		console.log(i);
+		if(i === -1){
 			item.quantity = 1;
-			console.log(item);
 			this.setState({
 				cart: this.state.cart.concat(item)
 			})
 		}
+		else{
+			cartCopy[i].quantity++;
+      		this.setState({ cart: cartCopy });
+		}
+
 	}
 
 	checkout = () => {
@@ -106,7 +124,7 @@ export default class App extends Component {
 					<h2>
 						Total: $
 						{this.state.cart.reduce(
-							(totalPrice, product)=> (totalPrice += product.price), 0
+							(totalPrice, product)=> (totalPrice += product.price * product.quantity), 0
 						)}
 					</h2>
 					
