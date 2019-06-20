bb13a6904dd73ae18eb98751525d0c3317cd602a
Rex Rojo <rojorex.bahillo@gmail.com>
2019-06-20 17:07:54 +0800

    submission

---
+++ src/App.js
@@ -0,0 +1,188 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      //products: [
+        //{
+          //id: 1,
+          //imageUrl: 'https://via.placeholder.com/150x150',
+          //title: 'fancy hat',
+          //price: 12.99,
+          //description: 'has a feather in it.',
+        //},
+        //{
+          //id: 2,
+          //imageUrl: 'https://via.placeholder.com/150x150',
+          //title: 'fancy car',
+          //price: 15750.45,
+          //description: 'goes vroom vroom.',
+        //},
+        //{
+          //id: 3,
+          //imageUrl: 'https://via.placeholder.com/150x150',
+          //title: 'simple rock',
+          //price: 5.0,
+          //description: 'it is a rock',
+        //},
+      //],
+      cart: [],
+      hats:[
+        {
+            id: 1,
+            title: "Fisherman's Hat",
+            description:
+              "Headgear commonly used by fishermen. Increases fishing skill marginally.",
+            price: 12.99,
+            imageUrl: "https://via.placeholder.com/150x150"
+          },
+          {
+            id: 2,
+            title: "Metal Hat",
+            description: "Uncomfortable, but sturdy.",
+            price: 8.99,
+            imageUrl: "https://via.placeholder.com/150x150"
+          }
+      ],
+      beachGear: [
+        {
+          id: 3,
+          title: "Tent",
+          description: "Portable shelter.",
+          price: 32.99,
+          imageUrl: "https://via.placeholder.com/150x150"
+        }
+      ]
+    };
+    this.handleCreditCardInput=this.handleCreditCardInput.bind(this);
+    this.handleAddressInput=this.handleAddressInput.bind(this);
+  }
+
+
+  addToCart(item) {
+    let cartCopy = this.state.cart.map(product => Object.assign({}, product));
+    let index = this.state.cart.findIndex(product => product.id === item.id);
+
+    if (index === -1) {
+      item = Object.assign({}, item, { quantity: 1 });
+      this.setState({ cart: [...this.state.cart, item] });
+    } else {
+      cartCopy[index].quantity++;
+      this.setState({ cart: cartCopy });
+    }
+  }
+
+
+  deleteFromCart(id) {
+    let cartCopy = this.state.cart.map(product => Object.assign({}, product));
+    let index = this.state.cart.findIndex(product => product.id === id);
+
+    if (cartCopy[index].quantity === 1) {
+      cartCopy.splice(index, 1);
+    } else if (cartCopy[index].quantity > 1) {
+      cartCopy[index].quantity--;
+    }
+
+    this.setState({ cart: cartCopy });
+  }
+
+  checkout = () => {
+    if (this.state.address.length > 0 && this.state.creditCard.length > 0) {
+      this.setState({ cart: [] });
+      alert("Purchase is complete!");
+    } else {
+      alert("Please fill out the required fields.");
+    }
+  };
+
+  handleAddressInput(e) {
+    this.setState({ address: e.target.value })
+  }
+
+  handleCreditCardInput(e) {
+    this.setState({ creditCard: e.target.value })
+  }
+
+  handleToggleView = () => {
+    this.setState({ cardView: !this.state.cardView });
+  };
+    
+render() {
+    return(
+        <div className="App">
+            <section className="products">
+                <h1>Products</h1>
+                    <button onClick={this.handleToggleView}>Toggle View</button>
+                    <h2>Hats</h2>
+                {this.state.hats.map(item=>(
+                    <div key={item.id} className={this.state.cardView ?"product" : "product-list"}>
+                        <img src={item.imageUrl}/>
+                        <div className="product-list">
+                        <h4>{item.title}</h4>
+                        <p>{item.description}</p>
+                        <p>{item.price}</p>
+                           
+                        <button onClick={()=> this.addToCart(item)}>Add to Cart</button>
+                        </div>
+                    </div>))}  
+
+
+                
+                <h2>Beach Gear</h2>
+                    {this.state.beachGear.map(item => (
+                        <div
+                        key={item.id}
+                        className={this.state.cardView ? "product" : "product-list"}>
+                           
+                            <img src={item.imageUrl} />
+                            <div className="product-list">
+                                <h4>{item.title}</h4>
+                                <p>{item.description}</p>
+                                <p>{item.price}</p>
+                                <button onClick={() => this.addToCart(item)}>Add to Cart
+                                </button>
+                                </div>
+                         </div>
+                            ))}
+            </section>
+
+
+            <section className="cart">
+                <h1>Cart</h1>
+                <h2>
+                    Total: $
+                    {this.state.cart.reduce(
+                        (totalPrice, product)=>(totalPrice +=product.price),0
+                    )}
+                </h2>
+                <div className="inputs">
+            <input
+              placeholder="address"
+              value={this.state.address}
+              onChange={this.handleAddressInput}
+            />
+            <input
+              placeholder="credit card number"
+              value={this.state.creditCard}
+              onChange={this.handleCreditCardInput}
+            />
+          </div>
+                <button onClick={this.checkout}>Checkout</button>
+                {this.state.cart.map(item=>(
+                    <div key={item.id} className="product">
+                        <div className="product-list">
+                        <img src={item.imageUrl}/>
+                        <h4>{item.title}</h4>
+                        <p>{item.description}</p>
+                        <p>{item.price}</p>
+                        <button onClick={()=> this.deleteFromCart(item.id)}>Remove from Cart</button>
+                        </div>
+                    </div>
+                ))}
+            </section>
+        </div>
+    );
+}
+}
