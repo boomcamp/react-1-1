73823db460509a5ffcd02569daf5b385f6d0dd34
Jan Patrick L. Bañares <janpatrick.banares@boom.camp>
2019-06-20 17:03:19 +0800

    added black diamond

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
+        cardView: true,
       cart: [],
       hats: [
         {
@@ -12,14 +13,16 @@ export default class App extends Component {
           description:
             "Headgear commonly used by fishermen. Increases fishing skill marginally.",
           price: 12.99,
-          imageUrl: "https://via.placeholder.com/150x150"
+          imageUrl: "https://via.placeholder.com/150x150",
+          quantity: 0
         },
         {
           id: 2,
           title: "Metal Hat",
           description: "Uncomfortable, but sturdy.",
           price: 8.99,
-          imageUrl: "https://via.placeholder.com/150x150"
+          imageUrl: "https://via.placeholder.com/150x150",
+          quantity: 0
         }
       ],
       shirt: [
@@ -29,7 +32,8 @@ export default class App extends Component {
           description:
             "Boomsourcing Official Shirt. Increases programming skills",
           price: 19.99,
-          imageUrl: "https://via.placeholder.com/150x150"
+          imageUrl: "https://via.placeholder.com/150x150",
+          quantity: 0
         }
       ],
       address: "",
@@ -37,9 +41,27 @@ export default class App extends Component {
     };
   }
   addToCart(product) {
-    this.setState({
-      cart: [...this.state.cart, product]
-    });
+    let cartCopy = this.state.cart.map(prod => Object.assign({}, prod));
+    let findIndex = this.state.cart.findIndex(prod=> prod.id === product.id);
+    if (findIndex === -1) {
+        product = Object.assign({}, product, { quantity: 1 });
+        this.setState({ cart: [...this.state.cart, product] });
+      } else {
+        cartCopy[findIndex].quantity++;
+        this.setState({ cart: cartCopy });
+      }
+  }
+  deleteProd(id) {
+    let cartCopy = this.state.cart.map(prod => Object.assign({}, prod));
+    let findIndex = this.state.cart.findIndex(prod => prod.id === id);
+
+    if (cartCopy[findIndex].quantity === 1) {
+      cartCopy.splice(findIndex, 1);
+    } else if (cartCopy[findIndex].quantity > 1) {
+      cartCopy[findIndex].quantity--;
+    }
+
+    this.setState({ cart: cartCopy });
   }
   checkOut = () => {
       if(this.state.address.length > 0 && this.state.creditCard.length > 0){
@@ -56,6 +78,7 @@ export default class App extends Component {
     
   };
   handleAddressInput=(e)=>{
+      
     this.setState({
         address: e.target.value
     });
@@ -65,14 +88,18 @@ export default class App extends Component {
         creditCard: e.target.value
     });
   }
+  handleToggleView = () => {
+    this.setState({ cardView: !this.state.cardView });
+  };
   render() {
     return (
       <div className="App">
         <section className="products">
           <h1>Products</h1>
+          <button onClick={this.handleToggleView}>Toggle View</button>
           <h2>Hats</h2>
           {this.state.hats.map(prod => (
-            <div key={prod.id} className="product">
+            <div key={prod.id}  className={this.state.cardView ? "product" : "product-list"}>
               <img src={prod.imageUrl} />
               <div className="prod-details">
                 <h4>{prod.title}</h4>
@@ -86,7 +113,8 @@ export default class App extends Component {
           ))}
           <h2>Shirts</h2>
           {this.state.shirt.map(prod => (
-            <div key={prod.id} className="product">
+            <div key={prod.id} className={this.state.cardView ? "product" : "product-list"}>
+                
               <img src={prod.imageUrl} />
               <div className="prod-details">
                 <h4>{prod.title}</h4>
@@ -106,7 +134,7 @@ export default class App extends Component {
           <h2>
             Total: $
             {this.state.cart.reduce(
-              (totalPrice, prod) => (totalPrice += prod.price),
+              (totalPrice, prod) =>   (totalPrice += prod.price * prod.quantity),
               0.0
             )}
           </h2>
@@ -133,7 +161,11 @@ export default class App extends Component {
               <div className="prod-details">
                 <h4>{prod.title}</h4>
                 <p>{prod.description}</p>
+                <p>{prod.quantity}</p>
                 <p>{prod.price}</p>
+                <button onClick={() => this.deleteProd(prod.id)}>
+                  Remove from Cart
+                </button>
               </div>
             </div>
           ))}
