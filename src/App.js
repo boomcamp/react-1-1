f9d8dcf73824c3a88e762e88a3e9f3c2e62450f7
John Paulo Barbin <john.barbin@boom.camp>
2019-06-20 17:23:29 +0800

    Part 4

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
                     'Headgear commonly used by fishermen. Increases fishing skill marginally.',
                   price: 12.99,
                   imageUrl: 'https://via.placeholder.com/150x150',
+                  quantity: 0
                 },
                 {
                   id: 2,
@@ -20,6 +21,7 @@ class App extends Component {
                   description: 'Uncomfortable, but sturdy.',
                   price: 8.99,
                   imageUrl: 'https://via.placeholder.com/150x150',
+                  quantity: 0
                 },
               ],
 
@@ -30,21 +32,43 @@ class App extends Component {
                   description: 'Portable shelter.',
                   price: 32.99,
                   imageUrl: 'https://via.placeholder.com/150x150',
+                  quantity: 0
                 },
               ],
 
             cart: [],
 
             address: "",
-            creditCard: ""
+            creditCard: "",
+            cardView: true
         };
     }
 
     addToCart(item) {
-        this.setState({
-            cart: [...this.state.cart, item]
-        });
+        let cartCopy = this.state.cart.map(product => Object.assign({}, product));
+        let index = this.state.cart.findIndex(product => product.id === item.id);
+
+        if (index === -1) {
+        item = Object.assign({}, item, { quantity: 1 });
+        this.setState({ cart: [...this.state.cart, item] });
+        } else {
+        cartCopy[index].quantity++;
+        this.setState({ cart: cartCopy });
+        }
     }
+
+    deleteFromCart(id) {
+        let cartCopy = this.state.cart.map(product => Object.assign({}, product));
+        let index = this.state.cart.findIndex(product => product.id === id);
+    
+        if (cartCopy[index].quantity === 1) {
+          cartCopy.splice(index, 1);
+        } else if (cartCopy[index].quantity > 1) {
+          cartCopy[index].quantity--;
+        }
+    
+        this.setState({ cart: cartCopy });
+      }
 
     checkout = () => {
         if (this.state.address.length > 0 && this.state.creditCard.length > 0) {
@@ -63,14 +87,19 @@ class App extends Component {
         this.setState({ creditCard: e.target.value });
     };
 
+    toggleView = () => {
+        this.setState({ cardView: !this.state.cardView });
+      };
+
     render() {
         return (
             <div className="App">
                 <section className="products">
                     <h1>Products</h1>
+                    <button onClick={this.toggleView}>Toggle View</button>
                     <h2>Hats</h2>
                         {this.state.hats.map(item => (
-                            <div key={item.id} className="product">
+                            <div key={item.id} className={this.state.cardView ? "product" : "product-list"}>
                             <img src={item.imageUrl} />
                             <div className="product-info">
                                 <h4>{item.title}</h4>
@@ -99,7 +128,7 @@ class App extends Component {
                     <h2>
                         Total: $
                         {this.state.cart.reduce(
-                        (totalPrice, product) => (totalPrice += product.price),
+                        (totalPrice, product) => (totalPrice += product.price * product.quantity),
                         0
                         )}
                     </h2>
@@ -121,8 +150,13 @@ class App extends Component {
                         <img src={item.imageUrl} />
                         <div className="product-info">
                             <h4>{item.title}</h4>
+                            <p>Quantity: {item.quantity}</p>
                             <p>{item.description}</p>
                             <p>{item.price}</p>
+
+                            <button onClick={() => this.deleteFromCart(item.id)}>
+                                Remove from Cart
+                            </button>
                         </div>
                         </div>
                     ))}
